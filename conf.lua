e05c95e92af14955a8079b864530577548b0a65a
Daniela Morais <dani.m@hotmail.co.uk>
2016-04-23 22:12:54 -0300

    Set version number

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-  t.title = "My first game"
-  t.version = "0.0.1"
+  t.title = "Star Trek Game"
+  t.version = "0.1.0"
   t.window.width = 480
   t.window.height = 800
   t.console = true
